58c927d64f4b863a83a4cff31c900cbebc5bc2ec
Steven Bready <stevenbready@gmail.com>
2017-12-15 12:08:33 -0700

    small change

--- src/components/Private/Private.js
+++ src/components/Private/Private.js
@@ -22,7 +22,7 @@ class Private extends Component {
                     <p>Email: {this.props.user.email}</p>
                     <p>ID: {this.props.user.auth_id}</p>
                     <h4>Available balance: {'$' + Math.floor((Math.random() + 1) * 100) + '.00'} </h4>
-                    <a href='http://localhost:3005/auth/logout'><button>Log out</button></a>
+                    <a href=${process.env.AUTH_LANDING_REDIRECT}><button>Log out</button></a>
                 </div>
                 :
                 <div className='info-container'>
